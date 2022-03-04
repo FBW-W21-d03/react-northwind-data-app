ce59858747ad09a5eb16252a6ca693ab39bf31f8
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-04 12:04:02 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import './App.scss';
 import customers from './data/customers.json';
+import orders from './data/orders.json';
 
 function App() {
-  return (
-    <div className="App">
-		  <h1>Northwind Data Site</h1>
-		  <div>There are {customers.length} customers.</div>
-		  <ul>
-			  {customers.filter(m => m.address.city === "London").map((customer, index) => {
-				  return (
-					  <li key={index}>{customer.companyName} ({customer.address.city})</li>
-				  )
-			  })}
-		  </ul>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Northwind Data Site</h1>
+			<div>There are {customers.length} customers.</div>
+			<ul>
+				{customers.filter(m => m.address.city === "London").map((customer, index) => {
+					return (
+						<li key={index}>{customer.companyName} ({customer.address.city})
+							<ul>
+								{orders.filter(m => m.customerID === customer.customerID).sort((a,b) => a.orderDate > b.orderDate).map(order => {
+									return (
+										<li>{order.orderID} - {order.orderDate.substring(0,10)}</li>
+									)
+								})}
+							</ul>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
