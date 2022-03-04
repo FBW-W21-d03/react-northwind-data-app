edf44e380d2b2f46c64fcfcd50f14db3fd42b125
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-04 11:17:26 +0100

    show customers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import customers from './data/customers.json';
 
 function App() {
   return (
     <div className="App">
 		  <h1>Northwind Data Site</h1>
+		  <div>There are {customers.length} customers.</div>
     </div>
   );
 }
